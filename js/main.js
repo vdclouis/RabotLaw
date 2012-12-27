7835a8538712ea604418cb59ebcca350cf602eec
Louis Van de Calseyde <Louis@mbsp.local>
2012-12-27 19:22:11 +0100

    commitke

--- js/main.js
+++ js/main.js
@@ -4,20 +4,6 @@ if(localStorage.getItem('rabotlaw-language') === null){
 	localStorage.setItem('rabotlaw-language', 'nl');
 }
 
-function getHtml(currentPage, jsonData, callback) {
-	$.ajax({
-		url: 'templates/'+ currentPage +'.html',
-		type: "GET",
-		dataType: "html",
-		success: function(template) {
-			var theMerged = Mustache.to_html(template, jsonData);
-			callback(theMerged);
-		},
-		error: function(){
-			console.log('htmlerror');
-		}
-	});
-}
 
 function supports_html5_storage() {
 	try {
@@ -28,46 +14,12 @@ function supports_html5_storage() {
 	}
 }
 
-var pageEvents = {
-	home: function () {
-		$('.law a').click( function () {
-			console.log("klik");
-			var currentPage = $(this).attr('rel');
-			$.ajax({
-				type: 'get',
-				contentType: 'application/json',
-				dataType: 'json',
-				url: 'data/texts-'+ currentLang +'.json',
-				success: function (jsonData) {
-					getHtml(currentPage, jsonData, function (html) {
-						$('#content').empty().append(html);
-						console.log('SUCK sex')
-					});
-				},
-				error: function() {
-					console.log('json error');
-				}
-			});
-		});
-	},
-	
-}
 
 $(function(){
-
 
 	$('#menuicon').click( function () {
 		$('.menu').toggle();
 	});
-
-	//supports_html5_storage();
-
-	if (Modernizr.localstorage) {
-		// window.localStorage is available!
-	} else {
-		// no native support for HTML5 storage :(
-		// maybe try dojox.storage or a third-party solution
-	}
 
 	//set data in localstorage
 	//page
@@ -87,86 +39,12 @@ $(function(){
 	//get data from ls
 	currentPage = localStorage.getItem('rabotlaw-page');
 	currentLang = localStorage.getItem('rabotlaw-language');
-
-	//load localStorage lang & page on first pageload
-	$.ajax({
-		type: 'GET',
-		contentType: 'application/json',
-		dataType: 'json',
-		url: 'data/texts-'+ currentLang +'.json',
-		success: function(jsonData) {
-			getHtml(
-				currentPage,
-				jsonData,
-				function (html) {
-					//EMPTY THE CONTENT AND APPEND THE HTML
-					$('#content').empty().append(html);
-					//LOAD THE PAGE FUNCTIONS
-					var currentPageEvents = pageEvents[currentPage];
-					currentPageEvents();
-				}
-			);
-		},
-		error: function() {
-			console.log('jsonerror');
-		}
-	});
-
-	//change language -> JSON files on click
-	$('#language-chooser a').click( function() {
-		var lang = $(this).attr('rel');
-		currentLang = lang;
-		currentPage = localStorage.getItem('rabotlaw-page');
-		console.log(currentPage);
-		$.ajax({
-			type: 'GET',
-			contentType: 'application/json',
-			dataType: 'json',
-			url: 'data/texts-'+ lang +'.json',
-			success: function(jsonData) {
-				getHtml(
-					currentPage,
-					jsonData,
-					function (html) {
-						$('#content').empty().append(html);
-						var currentPageEvents = pageEvents[currentPage];
-						currentPageEvents();
-					}
-				);
-			},
-			error: function() {
-				window.console.log('jsonerror');
-			}
-		});
-	});
-
-	//change page -> template on click
-	$('nav a').click(function(){
-		var currentPage = $(this).attr('rel');
-		$.ajax({
-			type: 'GET',
-			contentType: 'application/json',
-			dataType: 'json',
-			url: 'data/texts-'+ currentLang +'.json',
-			success: function(jsonData) {
-				getHtml(
-					currentPage,
-					jsonData,
-					function (html) {
-						$('#content').empty().append(html);
-						var currentPageEvents = pageEvents[currentPage];
-						currentPageEvents();
-					}
-				);
-			},
-			error: function() {
-				window.console.log('json get error');
-			}
-		});
-	}); //eo click
+	
 }); //eo doc ready
 
+function HomeCtrl($scope) {
 
+}
 
 
 
